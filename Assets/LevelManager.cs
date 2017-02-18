0c65f5986c7e2aff8a908cdbecca9f5eccf70ee2
Daniel Dauchy <dandauchy@gmail.com>
2017-02-18 18:19:06 -0500

    Sec04, Lecture 61: adding spatial audio

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	int currentIndex;
+
+	public void LoadNextScene () { 
+		currentIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(++currentIndex);
+	}
+
+	public void LoadPreviousScene () {
+		currentIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene (--currentIndex);
+	}
+
+	public void Quit() {
+		print ("Quitting Game");
+		Application.Quit();
+	}
+}
